81795a8efb88d0a3d4b1d062878e31c57d16d826
blackpink7442 <50853063+blackpink7442@users.noreply.github.com>
2021-06-27 22:37:59 +0800

    sulogin1

--- work1.py
+++ work1.py
@@ -1,13 +1,7 @@
 import requests
 import json
 url = "http://tsukumonet.ddns.net:21126/login"
-
-
-
-
-userid = "a1"
-userpa = "a1"
-
+signurl = "http://tsukumonet.ddns.net:21126/signup.html"
 
 headers = {'Content-Type': "application/json",
            'Host': "tsukumonet.ddns.net:21126",
@@ -15,10 +9,22 @@ headers = {'Content-Type': "application/json",
            'Referer': "http://tsukumonet.ddns.net:21126/login",
            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",}
 
-payload = {'account': userid,
-           'password': userpa,
-           }
+def sign(newuserid,newuserpa):
+    sign = {'account':newuserid,
+            'password':newuserpa,
+            }
+    response = requests.post(url = signurl , data = json.dumps(sign),headers=headers)
+    print(response.text)
 
-response = requests.post(url = url,data = json.dumps(payload),headers = headers)
-print(response.text)
-print(response.url)
+userid = "a1"
+userpa = "a1"
+
+def login(userid,userpa):    
+    payload = {'account': userid,
+                'password': userpa,
+            }
+    response = requests.post(url = url,data = json.dumps(payload),headers = headers)
+    print(response.text)
+
+
+
